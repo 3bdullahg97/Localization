554d6df9e5e33d891ca5334097199d80f48ed680
Abdullah Abdelqader <3bdullahg97@gmail.com>
2020-02-20 18:24:42 +0200

    messges.php

--- src/Resources/lang/ar/messages.php
+++ src/Resources/lang/ar/messages.php
@@ -1,19 +1,17 @@
 <?php
-$singular_name = 'العنصر';
-$plural_name = 'العناصر';
 
 return [
     'success' => [
-        'create' => 'تمت إضافة ' . $singular_name . ' بنجاح',
-        'update' => 'تم تعديل بيانات ' . $singular_name . ' بنجاح ',
-        'delete' => '{1} تم حذف ' . $singular_name . ' بنجاح|[2,*] تم حذف ' . $plural_name . ' بنجاح',
-        'restore' => '{1} تم استعادة ' . $singular_name . ' بنجاح|[2,*] تمت استعادة ' . $plural_name . ' بنجاح',
+        'create' => 'تمت الإضافة بنجاح',
+        'update' => 'تم التعديل بنجاح',
+        'delete' => 'تم الحذف بنجاح',
+        'restore' => 'تمت الإستعادة بنجاح',
     ],
     'fail' => [
-        'show'   => 'هذا ' . $singular_name . ' غير موجود',
-        'create' => 'حدث خطأ أثناء أضافة ' . $singular_name . '',
-        'update' => 'حدث خطأ أثناء تعديل بيانات ' . $singular_name . '',
-        'delete' => 'حدث خطأ أثناء حذف ' . $singular_name . '',
+        'show'   => 'هذا العنصر غير موجود',
+        'create' => 'حدث خطأ أثناء الإضافة',
+        'update' => 'حدث خطأ أثناء التعديل',
+        'delete' => 'حدث خطأ أثناء الحذف',
         'restore' => 'حدث خطأ أثناء الإستعادة'
     ]
 ];
